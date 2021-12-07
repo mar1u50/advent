68eccf43d2be049679fd27f7cb12e91c0611b82c
Marius Oancea <marius.oancea@seeq.com>
2021-12-07 14:29:37 +0200

    Day7 - part2

--- src/test/kotlin/Day7Test.kt
+++ src/test/kotlin/Day7Test.kt
@@ -2,21 +2,40 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.nield.kotlinstatistics.median
 import java.nio.file.Paths
+import java.util.stream.IntStream
 import kotlin.math.abs
 import kotlin.math.round
 
 fun parsePositions(positionsString: String): List<Int> = positionsString.split(",").toInts()
 
-fun computeOptimumFuel(positions: List<Int>) =
+fun computeOptimumFuelPart1(positions: List<Int>) =
     round(positions.median()).toInt().let { median -> positions.sumOf { abs(it - median) } }
-
-fun main() {
-    part1()
-}
 
 private fun part1() {
     val day7Input = Paths.get("src", "test", "kotlin", "day7.txt").toFile().readLines()[0]
-    print(computeOptimumFuel(parsePositions(day7Input)))
+    println("Part 1 - ${computeOptimumFuelPart1(parsePositions(day7Input))}")
+}
+
+fun stepCostP2(n: Int) = n * (n + 1) / 2;
+
+fun computeFuelPart2(positions: List<Int>, refPos: Int) =
+    positions.sumOf { stepCostP2(abs(it - refPos)) }
+
+fun computeOptimumFuelPart2(positions: List<Int>): Int {
+    val min = positions.minOrNull()!!
+    val max = positions.maxOrNull()!!
+
+    return (min..max).minOf { p -> computeFuelPart2(positions, p) }
+}
+
+private fun part2() {
+    val day7Input = Paths.get("src", "test", "kotlin", "day7.txt").toFile().readLines()[0]
+    println("Part 2 - ${computeOptimumFuelPart2(parsePositions(day7Input))}")
+}
+
+fun main() {
+    part1()
+    part2()
 }
 
 class Day7Test {
@@ -29,6 +48,24 @@ class Day7Test {
 
     @Test
     fun testComputeOptimumFuel() {
-        assertThat(computeOptimumFuel(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14))).isEqualTo(37)
+        assertThat(computeOptimumFuelPart1(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14))).isEqualTo(37)
+    }
+
+    @Test
+    fun testStepCostP2() {
+        assertThat(stepCostP2(0)).isEqualTo(0)
+        assertThat(stepCostP2(1)).isEqualTo(0 + 1)
+        assertThat(stepCostP2(2)).isEqualTo(0 + 1 + 2)
+        assertThat(stepCostP2(3)).isEqualTo(0 + 1 + 2 + 3)
+    }
+
+    @Test
+    fun testComputeFuelPart2() {
+        assertThat(computeFuelPart2(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14), 5)).isEqualTo(168)
+    }
+
+    @Test
+    fun testComputeOptimumFuelPart2() {
+        assertThat(computeOptimumFuelPart2(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14))).isEqualTo(168)
     }
 }
